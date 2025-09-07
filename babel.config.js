47b0190f8e87479968fa6528e3f1f30a12214b59
chandachewe10 <chewec03@gmail.com>
2025-09-07 15:07:47 -0700

    fix: fix issue on loading a Map

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,26 @@
+const path = require("path");
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin", 
+    ],
+    overrides: [
+      {
+        test: (fileName) => {
+       
+          return (
+            !fileName.includes(path.join("node_modules", "react-native-maps"))
+          );
+        },
+        plugins: [
+          ["@babel/plugin-transform-class-properties", { loose: true }],
+          ["@babel/plugin-transform-private-methods", { loose: true }],
+          ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+        ],
+      },
     ],
   };
 };
